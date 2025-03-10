3155a52a1c759792a23180f41996c32ea32de3a7
JordiLlopis10 <llopisgodinojordi@gmail.com>
2025-03-10 13:42:43 +0100

    Primer commit api-mongo

---
+++ app.py
@@ -0,0 +1,66 @@
+from bson import ObjectId
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+
+load_dotenv()
+
+app = Flask(__name__)
+
+uri = os.getenv("MONGO_URI")
+
+client = MongoClient(uri)
+db = client["test1"]
+
+@app.route("/")
+def show():
+    
+    data = list(db.users.find())
+    
+    for i in data:
+        i["_id"] = str(i["_id"])
+    
+    return jsonify({"mensaje":data})
+
+@app.route("/add", methods=["POST"])
+def add():
+    datos = request.get_json()
+    nombre = datos["nombre"]
+    edad = datos["edad"]
+    
+    if nombre and edad:
+        resultado = db.users.insert_one({"nombre":nombre,"edad":edad}).inserted_id
+        if resultado:
+            return jsonify({"Añadido correctamente":str(resultado)})
+        else:
+            return jsonify({"Mensaje":"No añadido, pringao"})
+    
+    return jsonify({"mensaje":nombre,"edad":edad})
+    
+@app.route("/user/<string:id>", methods=["PUT"])
+def actualizar(id):
+    datos = request.get_json()
+    nombre = datos["nombre"]
+    edad = datos["edad"]
+    if nombre and edad:
+        resultado = db.users.update_one({"_id":ObjectId(id)},{"$set":{"nombre":nombre,"edad":edad}})
+        if resultado:
+            return jsonify({"Mensaje":"Actualizado correctamente"})
+        else:
+            return jsonify({"Mensaje":"No actualizado, pringao"})
+            
+            
+@app.route("/delete/<string:id>", methods=["DELETE"])
+def delete(id):
+    
+    try:
+        dato = db.users.delete_one({"_id":ObjectId(id)})
+        return jsonify({"Mensaje":"Eliminado correctamente"})
+    
+    except Exception as e:
+        return e
+
+
+if __name__ == "__main__":
+    app.run()
